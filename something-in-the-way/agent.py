f5b9022690c43466e1f9885bf508685a96013190
Dmitry Popov <mitya1510@ya.ru>
2020-05-31 14:10:02 +0300

    add agent.py

--- something-in-the-way/agent.py
+++ something-in-the-way/agent.py
@@ -5,12 +5,12 @@ from torch import nn
 
 
 class StatePreprocessor(nn.Module):
-    def __init__(self, coord_range, field_values_range, 
-                       coord_emb_dim, field_emb_dim):
+    def __init__(self, coord_range, field_values_range, coord_emb_dim,
+                 field_emb_dim):
         super().__init__()
         self.coord_embedding = nn.Embedding(coord_range, coord_emb_dim)
         self.field_embedding = nn.Embedding(field_values_range, field_emb_dim)
-    
+
     def forward(self, coords, obses):
         """
         Parameters
@@ -24,46 +24,17 @@ class StatePreprocessor(nn.Module):
         """
         batch_size = coords.shape[0]
         coords_emb = self.coord_embedding(coords).view(batch_size, -1)
-        field_emb = self.field_embedding(obses.view(batch_size, -1)).view(batch_size, -1)
+        field_emb = self.field_embedding(obses.view(batch_size,
+                                                    -1)).view(batch_size, -1)
         return torch.cat([coords_emb, field_emb], dim=1)
-    
+
     def to_tensor(self, state):
         device = next(self.parameters()).device
         coord = torch.as_tensor([state.coordinate], device=device)
-        obs = torch.as_tensor([state.observation], dtype=torch.long, device=device)
+        obs = torch.as_tensor([state.observation],
+                              dtype=torch.long,
+                              device=device)
         return coord, obs
-
-
-# class StatePreprocessor(nn.Module):
-#     def __init__(self, values_range, embedding_dim, mean, std):
-#         super().__init__()
-#         self.mean = mean
-#         self.std = std
-#         self.embedding = nn.Embedding(values_range, embedding_dim)
-
-#     def forward(self, coords, obses):
-#         """
-#         Parameters
-#         ----------
-#         coords: torch.tensor (batch_size, 2)
-#         obses: torch.tensor (batch_size, receptive_field, receptive_field)
-        
-#         Returns
-#         -------
-#         torch.tensor (batch_size, embedding_dim * 2 + receptive_field ** 2)
-#         """
-#         batch_size = coords.shape[0]
-#         coords_emb = self.embedding(coords).view(batch_size, -1)
-#         obses = (obses.view(batch_size, -1) - self.mean) / self.std
-#         return torch.cat([coords_emb, obses], dim=1)
-
-#     def to_tensor(self, state):
-#         device = next(self.parameters()).device
-#         coord = torch.as_tensor([state.coordinate], device=device)
-#         obs = torch.as_tensor([state.observation],
-#                               dtype=torch.float,
-#                               device=device)
-#         return coord, obs
 
 
 class DQNAgent(nn.Module):
